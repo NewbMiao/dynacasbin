a2576eb434a8292cf5b7d6d850781e380926d0d4
newbmiao <newbvirgil@gmail.com>
2019-10-22 18:05:45 +0800

    support auto save

--- adapter.go
+++ adapter.go
@@ -1,16 +1,17 @@
-package dynamodbadapter
+package dynacasbin
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 
-	"github.com/google/uuid"
-
+	"crypto/md5"
 	"github.com/casbin/casbin/model"
 	"github.com/casbin/casbin/persist"
 	"github.com/guregu/dynamo"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
@@ -25,9 +26,9 @@ type (
 	}
 
 	CasbinRule struct {
-		ID    string `dynamo:"ID"`
-		PType string `dynamo:"PType"`
-		V0    string `dynamo:"V0"`
+		ID    string `dynamo:"ID"`    //md5 of rule
+		PType string `dynamo:"PType"` //hash key
+		V0    string `dynamo:"V0"`    //sort key
 		V1    string `dynamo:"V1"`
 		V2    string `dynamo:"V2"`
 		V3    string `dynamo:"V3"`
@@ -37,13 +38,25 @@ type (
 )
 
 // NewAdapter is the constructor for adapter
-func NewAdapter(config *aws.Config, ds string) *Adapter {
+func NewAdapter(config *aws.Config, ds string) (*Adapter, error) {
 	a := &Adapter{}
 	a.Config = config
 	a.DataSourceName = ds
-	a.Service = dynamodb.New(session.New(config), a.Config)
-	a.DB = dynamo.New(session.New(), a.Config)
-	return a
+	s, err := session.NewSession(config)
+	if err != nil {
+		return a, err
+	}
+	a.Service = dynamodb.New(s, a.Config)
+	s, _ = session.NewSession()
+	a.DB = dynamo.New(s, a.Config)
+	return a, err
+}
+
+// use md5(line) to prevent overwrites of an existing item
+func generateID(line CasbinRule) string {
+	data := []byte(fmt.Sprint(line))
+	has := md5.Sum(data)
+	return fmt.Sprintf("%x", has)
 }
 
 func loadPolicyLine(line CasbinRule, model model.Model) {
@@ -70,7 +83,7 @@ func loadPolicyLine(line CasbinRule, model model.Model) {
 	persist.LoadPolicyLine(lineText, model)
 }
 
-func (a *Adapter) LoadPolicy(model model.Model) {
+func (a *Adapter) LoadPolicy(model model.Model) error {
 	p, err := a.getAllItems()
 	if err != nil {
 		panic(err)
@@ -80,14 +93,11 @@ func (a *Adapter) LoadPolicy(model model.Model) {
 		loadPolicyLine(v, model)
 	}
 
-	return
+	return err
 }
 
 func savePolicyLine(ptype string, rule []string) CasbinRule {
-	id := uuid.New().String()
-	line := CasbinRule{
-		ID: id,
-	}
+	line := CasbinRule{}
 
 	line.PType = ptype
 	if len(rule) > 0 {
@@ -109,10 +119,17 @@ func savePolicyLine(ptype string, rule []string) CasbinRule {
 		line.V5 = rule[5]
 	}
 
+	//set md5 id
+	line.ID = generateID(line)
 	return line
 }
 
+//save all policy
 func (a *Adapter) SavePolicy(model model.Model) error {
+	//IMPORTANT: better not use， may recreate table failed under aws delete latency）
+	//If you still wanna use it, comment this line of code below.
+	return errors.New("not implemented")
+
 	a.DeleteTable()
 	a.CreateTable()
 
@@ -203,17 +220,83 @@ func (a *Adapter) DeleteTable() error {
 	return err
 }
 
+//This Err will return, if cond check is false
+func isConditionalCheckErr(err error) bool {
+	if ae, ok := err.(awserr.RequestFailure); ok {
+		return ae.Code() == "ConditionalCheckFailedException"
+	}
+	return false
+}
+
 // AddPolicy adds a policy rule to the storage.
 func (a *Adapter) AddPolicy(sec string, ptype string, rule []string) error {
-	return errors.New("not implemented")
+	item := savePolicyLine(ptype, rule)
+	err := a.DB.Table(a.DataSourceName).Put(item).If("attribute_not_exists(ID)").Run()
+	if isConditionalCheckErr(err) {
+		return nil
+	}
+	return err
 }
 
 // RemovePolicy removes a policy rule from the storage.
 func (a *Adapter) RemovePolicy(sec string, ptype string, rule []string) error {
-	return errors.New("not implemented")
+	item := savePolicyLine(ptype, rule)
+	return a.DB.Table(a.DataSourceName).Delete("ID", item.ID).Run()
 }
 
 // RemoveFilteredPolicy removes policy rules that match the filter from the storage.
+
+// IMPORTANT: Use ID as primary partition key and no sort key.
+// If has sort key, toggle the comment code of this func to map hash key & sort key.
 func (a *Adapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error {
-	return errors.New("not implemented")
+	res, err := a.getAllItems()
+	if err != nil {
+		return err
+	}
+	line := &CasbinRule{PType: ptype}
+
+	idx := fieldIndex + len(fieldValues)
+	if fieldIndex <= 0 && idx > 0 {
+		line.V0 = fieldValues[0-fieldIndex]
+	}
+	if fieldIndex <= 1 && idx > 1 {
+		line.V1 = fieldValues[1-fieldIndex]
+	}
+	if fieldIndex <= 2 && idx > 2 {
+		line.V2 = fieldValues[2-fieldIndex]
+	}
+	if fieldIndex <= 3 && idx > 3 {
+		line.V3 = fieldValues[3-fieldIndex]
+	}
+	if fieldIndex <= 4 && idx > 4 {
+		line.V4 = fieldValues[4-fieldIndex]
+	}
+	if fieldIndex <= 5 && idx > 5 {
+		line.V5 = fieldValues[5-fieldIndex]
+	}
+	items := make([]dynamo.Keyed, 0)
+	for _, item := range res {
+		if item.PType == line.PType {
+			if (line.V0 != "" && line.V0 != item.V0) ||
+				(line.V1 != "" && line.V1 != item.V1) ||
+				(line.V2 != "" && line.V2 != item.V2) ||
+				(line.V3 != "" && line.V3 != item.V3) ||
+				(line.V4 != "" && line.V4 != item.V4) ||
+				(line.V5 != "" && line.V5 != item.V5) {
+				continue
+			}
+			//items = append(items, dynamo.Keys{item.ID, item.PType}) //sort key: PType
+			items = append(items, dynamo.Keys{item.ID,}) // no sort key
+		}
+	}
+
+	if len(items) == 0 {
+		return nil
+	}
+	//cnt, err := a.DB.Table(a.DataSourceName).Batch("ID", "PType").Write().Delete(items...).Run() //sort key: PType
+	cnt, err := a.DB.Table(a.DataSourceName).Batch("ID").Write().Delete(items...).Run() // no sort key
+	if cnt == len(items) {
+		return nil
+	}
+	return err
 }
--- adapter_test.go
+++ adapter_test.go
@@ -1,50 +1,130 @@
-package dynamodbadapter
+package dynacasbin
 
 import (
-	"testing"
-
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/casbin/casbin"
-	"github.com/casbin/casbin/util"
+	"os"
+	"sort"
+	"strings"
+	"testing"
 )
+
+var config *aws.Config
+var a *Adapter
+var data [][]string
 
 func testGetPolicy(t *testing.T, e *casbin.Enforcer, res [][]string) {
 	myRes := e.GetPolicy()
 
-	if !util.Array2DEquals(res, myRes) {
-		t.Error("Policy: ", myRes, ", supposed to be ", res)
+	res1 := make([]string, len(res))
+	myRes1 := make([]string, len(myRes))
+	for k, v := range res {
+		res1[k] = fmt.Sprint(v)
+	}
+	for k, v := range myRes {
+		myRes1[k] = fmt.Sprint(v)
+	}
+	sort.Strings(res1)
+	sort.Strings(myRes1)
+
+	if fmt.Sprint(res1) != fmt.Sprint(myRes1) {
+		t.Error("Policy: ", myRes1, ", supposed to be ", res1)
 	}
 }
 
-func TestAdapter(t *testing.T) {
-	// load from CSV file
-	e := casbin.NewEnforcer("examples/rbac_model.conf", "examples/rbac_policy.csv")
+func init() {
+	// Initialize a DynamoDB adapter and use it in a Casbin enforcer:
+	config = &aws.Config{
+		Region:      aws.String("us-east-2"),
+		Credentials: credentials.NewSharedCredentials("", ""),
+	} // Your AWS configuration
+	ds := "casbin-rules"
+	var err error
+	a, err = NewAdapter(config, ds) // Your aws configuration and data source.
+	if err != nil {
+		panic(err)
+	}
 
-	dbEndpoint := "http://localhost:14045"
-	region := "ap-southeast-1"
+	data = [][]string{
+		{"alice", "data1", "read"},
+		{"bob", "data2", "write"},
+		{"data2_admin", "data2", "read"},
+		{"data2_admin", "data2", "write"},
+	}
+	for _, item := range data {
+		//init dynamodb if it's empty
+		err = a.AddPolicy("p", "p", item)
+		if err != nil {
+			if strings.Contains(err.Error(), "ResourceNotFoundException") {
+				_, err = a.CreateTable()
+				fmt.Println("table not exist, creating... pls try it later")
+				if err != nil {
+					fmt.Println("createTable error: " + err.Error())
+				}
+				os.Exit(1)
+			} else {
+				panic(err)
+			}
+		}
+	}
+}
 
-	a := NewAdapter(
-		&aws.Config{Endpoint: aws.String(dbEndpoint), Region: aws.String(region)},
-		"casbin-rules",
-	)
+func isPolicyExistInDB(rules [][3]string) bool {
+	res, err := a.getAllItems()
+	if err != nil {
+		panic("adapter getAllItems failed:" + err.Error())
+	}
+	isExist := false
+	for _, v := range res {
+		for _, item := range rules {
 
-	a.DeleteTable()
+			if v.V0 == item[0] && v.V1 == item[1] && v.V2 == item[2] {
+				isExist = true
+			}
+		}
+	}
 
-	m := e.GetModel()
+	return isExist
+}
 
-	a.SavePolicy(m)
+func TestLoadPolicy(t *testing.T) {
+	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+	res := make([][]string, 0)
+	items, err := a.getAllItems()
+	if err != nil {
+		panic("adapter getAllItems failed:" + err.Error())
+	}
+	for _, item := range items {
+		res = append(res, []string{item.V0, item.V1, item.V2})
+	}
+	testGetPolicy(t, e, res)
+}
 
-	e.ClearPolicy()
-	testGetPolicy(t, e, [][]string{})
+func TestAddPolicy(t *testing.T) {
+	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+	e.AddPolicy("jack", "data3", "read")
+	if !isPolicyExistInDB([][3]string{{"jack", "data3", "read"}}) {
+		t.Error("TestAddPolicy is not ok")
+	}
+}
 
-	// Load the policy from DB.
-	a.LoadPolicy(e.GetModel())
-	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
+func TestRemovePolicy(t *testing.T) {
+	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+	params := []interface{}{"alice", "data1", "read"}
+	e.RemovePolicy(params...)
 
-	a = NewAdapter(
-		&aws.Config{Endpoint: aws.String(dbEndpoint), Region: aws.String(region)},
-		"casbin-rules",
-	)
-	e = casbin.NewEnforcer("examples/rbac_model.conf", a)
-	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
+	if isPolicyExistInDB([][3]string{{"alice", "data1", "read"}}) {
+		t.Error("TestRemovePolicy is not ok")
+	}
+}
+
+func TestRemoveFilteredPolicy(t *testing.T) {
+	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+	e.RemoveFilteredPolicy(0, "data2_admin")
+	//check load is ok
+	if isPolicyExistInDB([][3]string{{"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}}) {
+		t.Error("TestRemoveFilteredPolicy is not ok")
+	}
 }
